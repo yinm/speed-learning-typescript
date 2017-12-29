a145e7283e5169ba3767f673ae733b77640e0784
yinm <yinm.cminor9@gmail.com>
2017-12-29 20:08:27 +0900

    Check tuple 2

--- ch2/sample.js
+++ ch2/sample.js
@@ -1,4 +1,3 @@
 var data = ['hoge', 10.355, false];
-console.log(data[0].substring(2));
-console.log(data[1].toFixed(1));
-console.log(data[2] === false);
+data.shift();
+console.log(data[0].substring((2)));
