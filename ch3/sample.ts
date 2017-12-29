1f3e2a47518f06063b558bd032c5e7d6103e82de
yinm <yinm.cminor9@gmail.com>
2017-12-29 21:39:43 +0900

    check type guards

--- ch3/sample.ts
+++ ch3/sample.ts
@@ -1,10 +1,5 @@
-function show(value: number | boolean): void {
-  if (typeof value === 'number') {
-    console.log(value.toFixed(0));
-  } else {
-    console.log(value ? '真' : '偽');
+function process(value: string | number) {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
   }
 }
-
-show(10.358);
-show(false);
