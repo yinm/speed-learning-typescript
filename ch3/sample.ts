6ac144ff12618c06caab7475101ba0bd29d0a910
yinm <yinm.cminor9@gmail.com>
2017-12-29 21:05:03 +0900

    Check arrow function

--- ch3/sample.ts
+++ ch3/sample.ts
@@ -1,6 +1,5 @@
-let triangle: (base: number, height: number) => number =
-  function (base: number, height: number): number {
+let triangle = (base: number, height: number): number => {
   return base * height / 2;
-  };
+};
 
 console.log(triangle(10, 5));
