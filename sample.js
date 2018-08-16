2e44c531585f4d15e632d1a8448dc85b0e34e621
yinm <yinm.cminor9@gmail.com>
2018-08-16 14:53:57 +0900

    Check class

--- sample.js
+++ sample.js
@@ -1,5 +1,12 @@
-function process(value) {
-    if (typeof value === 'string') {
-        return value.toUpperCase();
+var Person = /** @class */ (function () {
+    function Person(name, sex) {
+        this.name = name;
+        this.sex = sex;
     }
-}
+    Person.prototype.show = function () {
+        return this.name + "\u306F" + this.sex + "\u3067\u3059\u3002";
+    };
+    return Person;
+}());
+var p = new Person('理央', '女');
+console.log(p.show());
--- sample.ts
+++ sample.ts
@@ -1,5 +1,16 @@
-function process(value: string | number) {
-  if (typeof value === 'string') {
-    return value.toUpperCase()
+class Person {
+  name: string
+  sex: string
+
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+
+  show() {
+    return `${this.name}は${this.sex}です。`
   }
 }
+
+let p = new Person('理央', '女')
+console.log(p.show())
