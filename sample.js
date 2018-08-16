9408ef6d9a8465ac108712a0951b4fd1f9f63e63
yinm <yinm.cminor9@gmail.com>
2018-08-16 14:00:50 +0900

    Check default value of params

--- sample.js
+++ sample.js
@@ -1,9 +1,5 @@
 function showTime(time) {
-    if (time === undefined) {
-        return "\u73FE\u5728\u6642\u523B: " + (new Date()).toLocaleString();
-    }
-    else {
-        return "\u73FE\u5728\u6642\u523B: " + time.toLocaleString();
-    }
+    if (time === void 0) { time = new Date(); }
+    return "\u73FE\u5728\u6642\u523B: " + time.toLocaleString();
 }
 console.log(showTime());
--- sample.ts
+++ sample.ts
@@ -1,9 +1,4 @@
-function showTime(time?: Date): string {
-  if (time === undefined) {
-    return `現在時刻: ${(new Date()).toLocaleString()}`
-  } else {
-    return `現在時刻: ${time.toLocaleString()}`
-  }
+function showTime(time: Date = new Date()): string {
+  return `現在時刻: ${time.toLocaleString()}`
 }
-
 console.log(showTime())
