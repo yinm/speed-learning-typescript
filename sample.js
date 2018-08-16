e19ec710896e89ce449c1f33a4cc79f7d4c78e1e
yinm <yinm.cminor9@gmail.com>
2018-08-16 17:01:56 +0900

    Check extends

--- sample.js
+++ sample.js
@@ -1,16 +1,36 @@
-var Wings;
-(function (Wings) {
-    var MainApp;
-    (function (MainApp) {
-        var Hoge = /** @class */ (function () {
-            function Hoge() {
-            }
-            return Hoge;
-        }());
-        MainApp.Hoge = Hoge;
-        function foo() { }
-        MainApp.foo = foo;
-    })(MainApp = Wings.MainApp || (Wings.MainApp = {}));
-})(Wings || (Wings = {}));
-var h = new Wings.MainApp.Hoge();
-Wings.MainApp.foo();
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    Person.prototype.show = function () {
+        return this.name + "\u306F" + this.sex + "\u3067\u3059";
+    };
+    return Person;
+}());
+var BusinessPerson = /** @class */ (function (_super) {
+    __extends(BusinessPerson, _super);
+    function BusinessPerson() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    BusinessPerson.prototype.work = function () {
+        return this.name + "\u306F\u30C6\u30AD\u30D1\u30AD\u50CD\u304D\u307E\u3059\u3002";
+    };
+    return BusinessPerson;
+}(Person));
+var p = new BusinessPerson('理央', '女');
+console.log(p.show());
+console.log(p.work());
--- sample.ts
+++ sample.ts
@@ -1,7 +1,21 @@
-namespace Wings {
-  // 内側のnamespaceもexportする必要がある
-  export namespace MainApp {
-    export class Hoge()
-    export function foo() {}
+class Person {
+  constructor(
+    protected name: string,
+    protected sex: string
+  ) {
+  }
+
+  show(): string {
+    return `${this.name}は${this.sex}です`
   }
 }
+
+class BusinessPerson extends Person {
+  work(): string {
+    return `${this.name}はテキパキ働きます。`
+  }
+}
+
+let p = new BusinessPerson('理央', '女')
+console.log(p.show())
+console.log(p.work())
