524b425318046a52bfb5f295144f2d0a59d84aae
yinm <yinm.cminor9@gmail.com>
2018-08-16 15:08:50 +0900

    Check constructor

--- sample.ts
+++ sample.ts
@@ -1,17 +1,7 @@
 class Person {
-  private name: string
-  private sex: string
-
-  constructor(name, sex) {
-    this.name = name
-    this.sex = sex
-  }
-
-  public show(): string {
-    return `${this.name}は${this.sex}です`
+  constructor(
+    private name: string,
+    private sex: string
+  ) {
   }
 }
-
-let p = new Person('りお', '女')
-console.log(p.show())
-console.log(p.name)
