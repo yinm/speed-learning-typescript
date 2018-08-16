7fec3ffcb8ad9fb018f385dbb0814e5b32c1b0bc
yinm <yinm.cminor9@gmail.com>
2018-08-16 14:32:09 +0900

    Check type guard

--- sample.js
+++ sample.js
@@ -1,10 +1,5 @@
-function show(value) {
-    if (typeof value === 'number') {
-        console.log(value.toFixed(0));
-    }
-    else {
-        console.log(value ? '真' : '偽');
+function process(value) {
+    if (typeof value === 'string') {
+        return value.toUpperCase();
     }
 }
-show(10.53);
-show(false);
