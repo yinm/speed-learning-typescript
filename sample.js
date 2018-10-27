d611b8d74f1f85c11730061234ccdf2645cd7389
yinm <yinm.cminor9@gmail.com>
2018-10-27 21:07:49 +0900

    Check arrow function

--- sample.js
+++ sample.js
@@ -1,4 +1,2 @@
-var triangle = function (base, height) {
-    return base * height / 2;
-};
+var triangle = function (base, height) { return base * height / 2; };
 console.log(triangle(10, 5));
--- sample.ts
+++ sample.ts
@@ -1,5 +1,2 @@
-const triangle = (base: number, height: number): number => {
-  return base * height / 2
-}
-
+const triangle = (base: number, height: number): number => base * height / 2
 console.log(triangle(10, 5))
